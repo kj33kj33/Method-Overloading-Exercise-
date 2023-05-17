bd2d421e1843f5d2a65f404b5cd5b56a1e9bc5af
James <jamesmcdaniel33@hotmail.com>
2023-05-17 15:38:40 -0500

    initial commit

--- MethodOverloading/Program.cs
+++ MethodOverloading/Program.cs
@@ -2,9 +2,54 @@
 {
     public class Program
     {
+        public static int Add(int num1, int num2)
+        {
+            int answer = num1 + num2;
+            Console.WriteLine($"{num1} + {num2} = {answer}");
+            return answer;
+        }
+
+        public static decimal Add(decimal num1, decimal num2)
+        {
+            decimal answer = num1 + num2;
+            Console.WriteLine($"{num1} + {num2} = {answer}");
+            return answer;
+        }
+
+        public static string Add(int num1, int num2, bool dollars)
+        {
+            var answer = num1 + num2;
+            if (dollars == true)
+            {
+                if(answer == 1)
+                {
+                    string money = $"{answer} dollar";
+                    return money;
+                }
+                else
+                {
+                    string money = $"{answer} dollars";
+                    return money;
+                }
+            }
+            else
+            {
+                return $"{answer}";
+            }
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Add(1, 2);
+            Add(1.1m, 2.2m);
+
+            var answer1 = Add(1, 0, true);
+            var answer2 = Add(1, 2, true);
+            var answer3 = Add(2, 3, false);
+
+            Console.WriteLine(answer1);
+            Console.WriteLine(answer2);
+            Console.WriteLine(answer3);
         }
     }
 }
