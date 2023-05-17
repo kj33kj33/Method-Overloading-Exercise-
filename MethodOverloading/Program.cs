cafb507f0d3241de10f1cec158af7874e20c1791
James <jamesmcdaniel33@hotmail.com>
2023-05-17 15:51:43 -0500

    shorter methods

--- MethodOverloading/Program.cs
+++ MethodOverloading/Program.cs
@@ -2,54 +2,82 @@
 {
     public class Program
     {
+        //public static int Add(int num1, int num2)
+        //{
+        //    int answer = num1 + num2;
+        //    Console.WriteLine($"{num1} + {num2} = {answer}");
+        //    return answer;
+        //}
+
         public static int Add(int num1, int num2)
         {
-            int answer = num1 + num2;
-            Console.WriteLine($"{num1} + {num2} = {answer}");
-            return answer;
+            return num1 + num2;
         }
+
+        //public static decimal Add(decimal num1, decimal num2)
+        //{
+        //    decimal answer = num1 + num2;
+        //    Console.WriteLine($"{num1} + {num2} = {answer}");
+        //    return answer;
+        //}
 
         public static decimal Add(decimal num1, decimal num2)
         {
-            decimal answer = num1 + num2;
-            Console.WriteLine($"{num1} + {num2} = {answer}");
-            return answer;
+            return num1 + num2;
         }
 
-        public static string Add(int num1, int num2, bool dollars)
+        public static string Add(int num1, int num2, bool dollar)
         {
-            var answer = num1 + num2;
-            if (dollars == true)
+            var sum = num1 + num2;
+            if(dollar == true && sum == 1)
             {
-                if(answer == 1)
-                {
-                    string money = $"{answer} dollar";
-                    return money;
-                }
-                else
-                {
-                    string money = $"{answer} dollars";
-                    return money;
-                }
+                return $"{sum} dollar";
+            }
+            else if(dollar == true && sum != 1)
+            {
+                return $"{sum} dollars";
             }
             else
             {
-                return $"{answer}";
+                return $"{sum}";
             }
         }
 
+        //public static string Add(int num1, int num2, bool dollars)
+        //{
+        //    var answer = num1 + num2;
+        //    if (dollars == true)
+        //    {
+        //        if(answer == 1)
+        //        {
+        //            string money = $"{answer} dollar";
+        //            return money;
+        //        }
+        //        else
+        //        {
+        //            string money = $"{answer} dollars";
+        //            return money;
+        //        }
+        //    }
+        //    else
+        //    {
+        //        return $"{answer}";
+        //    }
+        //}
+
         static void Main(string[] args)
         {
-            Add(1, 2);
-            Add(1.1m, 2.2m);
-
-            var answer1 = Add(1, 0, true);
-            var answer2 = Add(1, 2, true);
-            var answer3 = Add(2, 3, false);
+            var answer1 = Add(1, 2);
+            var answer2 = Add(1.1m, 2.2m);
+            var answer3 = Add(1, 0, true);
+            var answer4 = Add(1, 2, true);
+            var answer5 = Add(2, 3, false);
 
             Console.WriteLine(answer1);
             Console.WriteLine(answer2);
             Console.WriteLine(answer3);
+            Console.WriteLine(answer4);
+            Console.WriteLine(answer5);
         }
     }
 }
